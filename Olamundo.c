5c68dbbd44166f73b46e3b38df525c332d3e6435
Felipe Felix <felipefxoc@gmail.com>
2016-04-21 20:23:12 +0000

    meu primeiro commit no github

---
+++ Olamundo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nOla Mundo!\n");
+    
+    return 0;
+}
---
+++ boanoite.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nBoa noite\n");
+    printf("Meu nome eh Felipe\n");
+    printf("Este eh meu segundo programa em C\n");
+    return 0;
+}
