8ee44ad7f6fdca55672f902cb39d0e3f9a989b34
Felipe Felix <felipefxoc@gmail.com>
2016-05-09 16:50:32 +0000

    testando o char

---
+++ charac.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char lt;
+    
+    printf("Entre com uma letra:");
+    lt = getchar();
+    
+    printf("caracter lido = %c\n", lt);
+    printf("caracter lido = %d\n", lt);
+    return 0;
+}
